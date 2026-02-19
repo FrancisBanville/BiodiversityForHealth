77efd184e6da387ae36c5f08e7ff20c90ccdee9c
Francis Banville <francis.banville@umontreal.ca>
2026-02-19 18:51:46 -0500

    :tada: update figures 2, 3 and 4

--- code/data_analysis_and_visualization.R
+++ code/data_analysis_and_visualization.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(forcats)
+library(grid)
 library(ggplot2)
+library(png)
 library(readr)
 library(rphylopic)
 library(tidyr)
@@ -275,7 +277,7 @@ table(df_clean$action_AT6, df_clean$use_AT6)
 #### prepare figures ####
 
 # One Health colors
-colors <- c("#F7921C", "#EC008B", "#90268E", "#1976BC", "#36B449", "#D6DF22", "grey")
+colors <- c("#F7921C", "#EC008B", "#90268E", "#1976BC", "#36B449", "#D6DF22", "darkgrey")
 
 # PhyloPics
 # Homo sapiens (Action track 1)
@@ -302,6 +304,9 @@ AT5_img <- get_phylopic(uuid = AT5_uuid)
 AT6_uuid <- get_uuid(name = "Carpobrotus edulis")
 AT6_img <- get_phylopic(uuid = AT6_uuid)
 
+# OHJPA logo
+img_OHJPA <- readPNG("images/OHJPA.png")
+img_OHJPA <- rasterGrob(img_OHJPA, interpolate=TRUE)
 
 # prepare dataset to analyse the links between indicators and human, animal, plant, and environmental health
 
@@ -380,7 +385,8 @@ df_long_GAP <- df_clean %>%
          use_AT3,
          use_AT4,
          use_AT5,
-         use_AT6) %>% 
+         use_AT6,
+         use_ATs) %>% 
   
   # long format needed since indicators are associated with multiple action tracks
   pivot_longer(cols = !GAP,
@@ -394,7 +400,8 @@ df_long_GAP <- df_clean %>%
                                    "Action track 3" = "use_AT3",
                                    "Action track 4" = "use_AT4",
                                    "Action track 5" = "use_AT5",
-                                   "Action track 6" = "use_AT6")) 
+                                   "Action track 6" = "use_AT6", 
+                                   "All action tracks" = "use_ATs")) 
 
 
 
@@ -490,7 +497,7 @@ df_usability <- df_usability %>%
 ggplot() + 
   
   # add grey bars representing the total number of indicators
-  geom_bar(data = df_usability, aes(y=tot, x=action_track), fill = "grey", alpha = 0.1,
+  geom_bar(data = df_usability, aes(y=tot, x=action_track), fill = "grey", alpha = 0.2,
            position="stack", stat="identity") +
   
   # add color bars representing the number of usable indicators for each action track
@@ -509,7 +516,7 @@ ggplot() +
                    limits = rev(levels(df_long_cat$action_track))) +
   
   # format y label
-  scale_y_continuous(limits = c(0,210), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(0,230), expand = c(0, 0)) +
   
   # use One Health colors
   scale_fill_manual(values = colors) +
@@ -537,9 +544,12 @@ ggplot() +
   add_phylopic(img = AT5_img, x = 3, y = 50, ysize = 0.6) +
   add_phylopic(img = AT6_img, x = 2, y = 173, ysize = 0.7) +
   
+  # add OHJPA logo
+  annotation_custom(img_OHJPA, xmin=0, xmax=2, ymin=188, ymax=202) +
+
   # add proportions of usable indicators for each action track
   annotate("text", x = c(7, 6, 5, 4, 3, 2, 1), 
-           y = c(130, 125, 103, 138, 65, 188, 195),
+           y = c(130, 125, 103, 138, 65, 188, 210),
            label = paste0(round(df_usability$prop_usable, 0), "%"))
 
 # save figure
@@ -552,44 +562,86 @@ ggsave("figures/usability_all.png",
 
 #### Figure: bar plot (proportion of usable indicators per action track and category)
 
-# number of indicators in each category
-df_total_cat <- df_long_cat %>%  
+# total number of indicators in each category and action track
+n_all_cat <- df_long_cat %>%  
   group_by(category, action_track) %>% 
-  count()
+  count() %>% 
+  rename(tot = n)
 
-# number of usable indicators in each category
-df_usable_cat <- df_long_cat %>%  
-  filter(usability == "usable") %>% 
+# number of directly usable indicators in each category and action track
+n_direct_cat <- df_long_cat %>%  
+  filter(usability == "directly usable") %>% 
   group_by(category, action_track) %>% 
-  count()
+  count() %>% 
+  rename(n_direct = n)
+
+# number of indicators usable after adaptation in each category and action track
+n_adapt_cat <- df_long_cat %>%  
+  filter(usability == "usable after adaptation") %>% 
+  group_by(category, action_track) %>% 
+  count() %>% 
+  rename(n_adapt = n)
+
+# merge data frames
+df_usability_cat <- n_all_cat %>% 
+  left_join(n_direct_cat, by = c("category", "action_track")) %>% 
+  left_join(n_adapt_cat, by = c("category", "action_track"))
+
+# total number of usable indicators in each category and action track
+df_usability_cat <- df_usability_cat %>% 
+  mutate(n_usable = n_direct + n_adapt)
+
+# proportion of usable indicators for each category and action track
+df_usability_cat <- df_usability_cat %>% 
+  mutate(prop_direct = n_direct / tot) %>% # directly usable
+  mutate(prop_adapt = n_adapt / tot) %>%  # usable after adaptation
+  mutate(prop_usable = n_usable / tot) %>% # both 
+  mutate(prop_tot = 1)
+
+# number of indicators for each category
+n_cat <- df_clean %>%  
+  group_by(category) %>% 
+  count() 
 
 
 ggplot() + 
   
-  # add grey bars representing the total number of indicators
-  geom_bar(data = df_total_cat, aes(y=n, x=action_track), fill = "grey", alpha = 0.6,
+  # add grey bars representing the maximum proportion (1)
+  geom_bar(data = df_usability_cat, aes(y=prop_tot, x=action_track), fill = "grey", alpha = 0.2,
            position="stack", stat="identity") +
   
-  # add color bars representing the number of usable indicators for each action track
-  geom_bar(data = df_usable_cat, aes(fill=action_track, y=n, x=action_track), 
+  # add color bars representing the proportion of usable indicators for each action track
+  geom_bar(data = df_usability_cat, aes(fill=action_track, y=prop_usable, x=action_track), 
+           alpha = 0.4, position="stack", stat="identity") +
+  
+  # add color bars representing the proportion of directlt usable indicators for each action track
+  geom_bar(data = df_usability_cat, aes(fill=action_track, y=prop_direct, x=action_track), 
            position="stack", stat="identity") +
   
   # add icons
-  add_phylopic(img = AT1_img, x = 1, y = 8, ysize = 12) +
-  add_phylopic(img = AT2_img, x = 2, y = 8, ysize = 12) +
-  add_phylopic(img = AT3_img, x = 3, y = 8, ysize = 7) +
-  add_phylopic(img = AT4_img, x = 4, y = 8, ysize = 12) +
-  add_phylopic(img = AT5_img, x = 5, y = 8, ysize = 10) +
-  add_phylopic(img = AT6_img, x = 6, y = 8, ysize = 12) +
+  add_phylopic(img = AT1_img, x = 1, y = 0.1, ysize = 0.15) +
+  add_phylopic(img = AT2_img, x = 2, y = 0.1, ysize = 0.15) +
+  add_phylopic(img = AT3_img, x = 3, y = 0.07, ysize = 0.07) +
+  add_phylopic(img = AT4_img, x = 4, y = 0.1, ysize = 0.15) +
+  add_phylopic(img = AT5_img, x = 5, y = 0.1, ysize = 0.1) +
+  add_phylopic(img = AT6_img, x = 6, y = 0.1, ysize = 0.15) +
+  
+  # add OHJPA logo
+  annotation_custom(img_OHJPA, xmin=6.2, xmax=7.8, ymin=0.03, ymax=0.16) +
   
   # split by indicator category
-  facet_wrap(~category) +
+  facet_wrap(~category, 
+             labeller = labeller(category =
+                                   c("Headline indicators" = paste0("Headline indicators (n = ", n_cat[1, 2] ,")"),
+                                     "Binary indicators" = paste0("Binary indicators (n = ", n_cat[2, 2] ,")"),
+                                     "Component indicators" = paste0("Component indicators (n = ", n_cat[3, 2] ,")"),
+                                     "Complementary indicators" = paste0("Complementary indicators (n = ", n_cat[4, 2] ,")")))) +
   
   # format x label
   scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
   
   # format y label 
-  scale_y_continuous(limits = c(0,125), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(0,1), expand = c(0, 0)) +
   
   # use One Health colors
   scale_fill_manual(values = colors) +
@@ -606,7 +658,7 @@ ggplot() +
         legend.position = "none") +
   
   # rename y axis
-  ylab("Number of indicators")
+  ylab("Proportion of indicators")
 
 
 ggsave("figures/usability_categories.png",
@@ -618,44 +670,101 @@ ggsave("figures/usability_categories.png",
 
 #### Figure: bar plot (proportion of usable indicators per action track and GAP category)
 
-# number of indicators in each category
-df_total_GAP <- df_long_GAP %>%  
+# total number of indicators in each GAP category and action track
+n_all_GAP <- df_long_GAP %>%  
   group_by(GAP, action_track) %>% 
-  count()
+  count() %>% 
+  rename(tot = n)
 
-# number of usable indicators in each category
-df_usable_GAP <- df_long_GAP %>%  
-  filter(usability == "usable") %>% 
+# number of directly usable indicators in each GAP category and action track
+n_direct_GAP <- df_long_GAP %>% 
   group_by(GAP, action_track) %>% 
-  count()
+  filter(usability == "directly usable") %>% 
+  count() %>% 
+  rename(n_direct = n)
+
+# number of indicators usable after adaptation in each GAP category and action track
+n_adapt_GAP <- df_long_GAP %>%  
+  group_by(GAP, action_track) %>% 
+  filter(usability == "usable after adaptation") %>% 
+  count() %>% 
+  rename(n_adapt = n)
+
+# merge data frames
+df_usability_GAP <- n_all_GAP %>% 
+  left_join(n_direct_GAP, by = c("GAP", "action_track")) %>% 
+  left_join(n_adapt_GAP, by = c("GAP", "action_track"))
+
+# change NAs to 0s
+df_usability_GAP <- df_usability_GAP %>% 
+  replace_na(list(n_direct = 0, n_adapt = 0))
+  
+# total number of usable indicators in each category and action track
+df_usability_GAP <- df_usability_GAP %>% 
+  mutate(n_usable = n_direct + n_adapt)
+
+# proportion of usable indicators for each category and action track
+df_usability_GAP <- df_usability_GAP %>% 
+  mutate(prop_direct = n_direct / tot) %>% # directly usable
+  mutate(prop_adapt = n_adapt / tot) %>%  # usable after adaptation
+  mutate(prop_usable = n_usable / tot) %>% # both 
+  mutate(prop_tot = 1)
+
+# number of indicators for each category
+n_GAP <- df_clean %>%  
+  group_by(GAP) %>% 
+  count() 
 
 
 ggplot() + 
   
   # add grey bars representing the total number of indicators
-  geom_bar(data = df_total_GAP, aes(y=n, x=action_track), fill = "grey", alpha = 0.6,
+  geom_bar(data = df_usability_GAP, aes(y=prop_tot, x=action_track), fill = "grey", alpha = 0.2,
            position="stack", stat="identity") +
   
   # add color bars representing the number of usable indicators for each action track
-  geom_bar(data = df_usable_GAP, aes(fill=action_track, y=n, x=action_track), 
-           position="stack", stat="identity") +
+  geom_bar(data = df_usability_GAP, aes(fill=action_track, y=prop_usable, x=action_track), 
+           alpha = 0.4, position="stack", stat="identity") +
   
+  # add color bars representing the number of directly usable indicators for each action track
+  geom_bar(data = df_usability_GAP, aes(fill=action_track, y=prop_direct, x=action_track), 
+           position="stack", stat="identity") +
+
   # add icons
-  add_phylopic(img = AT1_img, x = 1, y = 8, ysize = 12) +
-  add_phylopic(img = AT2_img, x = 2, y = 8, ysize = 12) +
-  add_phylopic(img = AT3_img, x = 3, y = 8, ysize = 7) +
-  add_phylopic(img = AT4_img, x = 4, y = 8, ysize = 12) +
-  add_phylopic(img = AT5_img, x = 5, y = 8, ysize = 10) +
-  add_phylopic(img = AT6_img, x = 6, y = 8, ysize = 12) +
+  add_phylopic(img = AT1_img, x = 1, y = 0.1, ysize = 0.2) +
+  add_phylopic(img = AT2_img, x = 2, y = 0.1, ysize = 0.2) +
+  add_phylopic(img = AT3_img, x = 3, y = 0.1, ysize = 0.1) +
+  add_phylopic(img = AT4_img, x = 4, y = 0.1, ysize = 0.2) +
+  add_phylopic(img = AT5_img, x = 5, y = 0.1, ysize = 0.15) +
+  add_phylopic(img = AT6_img, x = 6, y = 0.1, ysize = 0.2) +
+  
+  # add OHJPA logo
+  annotation_custom(img_OHJPA, xmin=6, xmax=8, ymin=0.03, ymax=0.2) +
   
   # split by GAP category
-  facet_wrap(~GAP, nrow = 5, ncol = 3) +
+  facet_wrap(~GAP, nrow = 5, ncol = 3,
+             labeller = labeller(GAP =
+                                   c("Access and benefit-sharing" = paste0("Access and benefit-sharing (n = ", n_GAP[1, 2] ,")"),
+                                     "Agriculture, aquaculture, fisheries and forestry" = paste0("Agriculture, aquaculture, fisheries and forestry (n = ", n_GAP[2, 2] ,")"),
+                                     "Biosafety and biotechnology" = paste0("Biosafety and biotechnology (n = ", n_GAP[3, 2] ,")"),
+                                     "Climate change" = paste0("Climate change (n = ", n_GAP[4, 2] ,")"),
+                                     "Consumption" = paste0("Consumption (n = ", n_GAP[5, 2] ,")"),
+                                     "Invasive alien species" = paste0("Invasive alien species (n = ", n_GAP[6, 2] ,")"),
+                                     "Knowledge and engagement of people" = paste0("Knowledge and engagement of people (n = ", n_GAP[7, 2] ,")"),
+                                     "Land and sea use" = paste0("Land and sea use (n = ", n_GAP[8, 2] ,")"),
+                                     "Mainstreaming" = paste0("Mainstreaming (n = ", n_GAP[9, 2] ,")"),
+                                     "Means of implementation" = paste0("Means of implementation (n = ", n_GAP[10, 2] ,")"),
+                                     "Nature’s contributions to people" = paste0("Nature’s contributions to people (n = ", n_GAP[11, 2] ,")"),
+                                     "Pollution" = paste0("Pollution (n = ", n_GAP[12, 2] ,")"),
+                                     "Species management" = paste0("Species management (n = ", n_GAP[13, 2] ,")"),
+                                     "Urban areas" = paste0("Urban areas (n = ", n_GAP[14, 2] ,")"),
+                                     "No category" = paste0("No category (n = ", n_GAP[15, 2] ,")")))) +
   
   # format x label
   scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
   
   # format y label 
-  scale_y_continuous(limits = c(0,55), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(0,1), expand = c(0, 0)) +
   
   # use One Health colors
   scale_fill_manual(values = colors) +
@@ -672,9 +781,9 @@ ggplot() +
         legend.position = "none") +
   
   # rename y axis
-  ylab("Number of indicators")
+  ylab("Proportion of indicators")
 
 
 ggsave("figures/usability_GAP.png",
-       width = 8, height = 6, dpi = 800, 
+       width = 10, height = 8, dpi = 800, 
        units = "in", device='png')
